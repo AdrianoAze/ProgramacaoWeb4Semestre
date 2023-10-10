f95382c3dcbbafab172a76d07e8c0f02e1bcd15d
Adriano Aze <adrianoskairos@gmail.com>
2023-10-09 21:45:02 -0400

    nova versao

--- Segunda-feira Aula Thiago/02-10-2023/server.js
+++ Segunda-feira Aula Thiago/02-10-2023/server.js
@@ -1,11 +1,17 @@
-const express = require ('express');
-const app = express ();
+const express = require('express')
+const app = express()
 app.use(express.static('public'));
-app.use('/css', express.static(__dirname + 'public/css'));
-app.set('view engine', 'ejs');
-app.get("/", function(request, response) {
+app.use('/css', 
+express.static(__dirname + 'public/css'))
+app.set('view engine','ejs')
+app.get("/", function(request, response){
     response.render('pages/index')
 })
-
-app.listen(8080);
-console.log('Tá conectando');
+app.get("/sobre", function(request, response){
+    response.render('pages/about')
+})
+app.get("/contato", function(request, response){
+    response.render('pages/contact')
+})
+app.listen(8080)
+console.log('Server is running');
